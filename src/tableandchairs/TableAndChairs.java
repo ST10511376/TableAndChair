27f4aa0800fbab7874f4b23f41ade4174541037e
ST10511376 <ST10511376@myemeris.edu.za>
2026-02-25 09:40:26 +0200

    Table&Chair submission

---
+++ src/tableandchairs/TableAndChairs.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tableandchairs;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class TableAndChairs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.print("X                                         X\nX                 *******                 X\nX                    |                    X\nX            X X X X X X X X X            X\nX");System.out.println(" X X X X    X               X    X X X X X\nX       X    X     JAVA      X    X       X\nX       X    X               X    X       X\nX       X    X               X    X       X        "); 
+        System.out.println("-------------------------------------------"); 
+        System.out.println("              TABLE & CHAIRS");
+               
+
+    }
+    
+}
