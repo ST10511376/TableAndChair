69d24f3a91e4f2c0039be6b753da351ac265a594
ST10511376 <ST10511376@myemeris.edu.za>
2026-02-27 08:06:46 +0200

    Table&Chairs 2nd

--- src/tableandchairs/TableAndChairs.java
+++ src/tableandchairs/TableAndChairs.java
@@ -15,7 +15,8 @@ public class TableAndChairs {
      */
     public static void main(String[] args) 
     {
-        System.out.print("X                                         X\nX                 *******                 X\nX                    |                    X\nX            X X X X X X X X X            X\nX");System.out.println(" X X X X    X               X    X X X X X\nX       X    X     JAVA      X    X       X\nX       X    X               X    X       X\nX       X    X               X    X       X        "); 
+        System.out.print("X                                         X\nX                 *******                 X\nX                    |                    X\nX            X X X X X X X X X            X\nX");
+        System.out.println(" X X X X    X               X    X X X X X\nX       X    X     JAVA      X    X       X\nX       X    X               X    X       X\nX       X    X               X    X       X        "); 
         System.out.println("-------------------------------------------"); 
         System.out.println("              TABLE & CHAIRS");
                
